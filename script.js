2e7c2a1585a450e19262c0da1d05e819894b7a50
Seohui Choe <chelsea.choe@gmail.com>
2019-12-10 22:23:55 -0500

    added functions for adding todos, displaying todos retrieved from local storage, and color-coding input field in terms of current time

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
 $(document).ready(function () {
+
+    var startDate = moment();
+
     var values = [
         {
-            timeVal: moment().startOf('day').add(9, 'hours'),
+            timeVal: null,
             timeEl: $("#time1"),
-            todoEl: $("#todo1")
+            todoEl: $("#todo1"),
         },
         {
-            timeVal: moment().startOf('day').add(10, 'hours'),
+            timeVal: null,
             timeEl: $("#time2"),
-            todoEl: $("#todo2")
+            todoEl: $("#todo2"),
         },
         {
-            timeVal: moment().startOf('day').add(11, 'hours'),
+            timeVal: null,
             timeEl: $("#time3"),
             todoEl: $("#todo3")
         },
         {
-            timeVal: moment().startOf('day').add(12, 'hours'),
+            timeVal: null,
             timeEl: $("#time4"),
             todoEl: $("#todo4")
         },
         {
-            timeVal: moment().startOf('day').add(13, 'hours'),
+            timeVal: null,
             timeEl: $("#time5"),
             todoEl: $("#todo5")
         },
         {
-            timeVal: moment().startOf('day').add(14, 'hours'),
+            timeVal: null,
             timeEl: $("#time6"),
             todoEl: $("#todo6")
         },
         {
-            timeVal: moment().startOf('day').add(15, 'hours'),
+            timeVal: null,
             timeEl: $("#time7"),
             todoEl: $("#todo7")
         },
         {
-            timeVal: moment().startOf('day').add(16, 'hours'),
+            timeVal: null,
             timeEl: $("#time8"),
             todoEl: $("#todo8")
         },
         {
-            timeVal: moment().startOf('day').add(17, 'hours'),
+            timeVal: null,
             timeEl: $("#time9"),
             todoEl: $("#todo9")
         }
@@ -49,37 +52,81 @@ $(document).ready(function () {
 
     // Display current day & date and times for the scheduler
     function setTimes() {
-        var today = moment().format('dddd');
-        var currentDate = moment().format('MMMM Do YYYY');
-        $("#today").text(today + ", " + currentDate);
+        let currentDay = startDate.format('dddd');
+        let currentDate = startDate.format('MMMM Do YYYY');
+        let today = startDate.format("MMM Do YY");
+
+        $("#today").text(currentDay + ", " + currentDate);
+
+        var hours = 9;
+        
+        values.forEach(function (element) {
+            let temp = startDate.clone().startOf('day').add(hours, 'hours');
+            // element.timeVal.text(startDate.startOf('day').add(hours, 'hours'));
+            element.timeVal = temp;
+            // console.log(element.timeVal);
+            element.timeEl.text(temp.format("h A"));
+            // element.timeEl.text(element.timeVal.format("h A"));
+            element.timeEl.attr("value", today);
+            hours++;
+            
+        })
 
         values.forEach(function (element) {
-            element.timeEl.text(element.timeVal.format("h A"));
+            console.log(element.timeVal);
         })
     }
 
     // Dinamically display colors associated with time
     function colorCode() {
         values.forEach(function (element) {
-            if (element.timeVal.format("H") - moment().format("H") < 0) {
+            element.todoEl.css("height", "80px");
+
+            let then = element.timeVal.format("MMM Do YY");
+            let now = moment().format("MMM Do YY");
+            
+            // if (then.isBefore(now) == true) {
+            //     element.todoEl.css("background-color", "gray");
+            // }
+            // else if (then.diff(now, "hours") < 1) {
+            //     element.todoEl.css("background-color", "red");
+            //     console.log(then.diff(now, "minutes") < 60)
+            // }
+
+            // console.log(element.timeVal.format("H"));
+            // console.log(startDate.format("H"));
+            console.log(now);
+            console.log(then);
+            console.log(now > then);
+            
+
+            // if(now > then) {
+            //     console
+            // }
+
+            if (element.timeVal.format("H") - startDate.format("H") < 0) {
                 element.todoEl.css("background-color", "gray");
             }
-            else if (element.timeVal.format("H") - moment().format("H") == 0) {
+            
+            else if (element.timeVal.format("H") - startDate.format("H") == 0) {
                 element.todoEl.css("background-color", "red");
             }
             else {
                 element.todoEl.css("background-color", "green");
+
             }
         })
     }
 
     var arr = [];
     var storedValues = JSON.parse(localStorage.getItem("data"));
+
     // If a button is clicked, input will be saved in local storage
-    $(".btn").on("click", function (e) {
-        let userInput = $(this).closest('div.row').find("input[class='todo']").val();
-        let timeForTodo = $(this).closest('div.row').find('.time').text();
-        var storedValues = JSON.parse(localStorage.getItem("data"));
+    $(".btn-primary").on("click", function (e) {
+        let userInput = $(this).closest('div.row').find("input[class='form-control']").val();
+        // let timeForTodo = $(this).closest('div.row').find('.time').text() + ", " + today;
+        let timeTargeted = $(this).closest('div.row').find('.time');
+        let timeForTodo = timeTargeted.text() + ", " + timeTargeted.attr("value");
 
         if (storedValues !== null) arr = storedValues;
 
@@ -88,27 +135,45 @@ $(document).ready(function () {
         }
         arr.push({ "time": timeForTodo, "todo": userInput });
         localStorage.setItem("data", JSON.stringify(arr));
-
         console.log(timeForTodo);
         console.log(userInput);
     })
 
+    // Display stored todos in input fields even when the application is refreshed
     function displayTodos() {
         values.forEach(function (element) {
-            console.log(element.timeEl.text());
+            let timeForTodo = element.timeEl.text() + ", " + element.timeEl.attr("value");
+            let todoDisplayed = element.todoEl;
+            todoDisplayed.val("");
             if (storedValues !== null) {
                 for (var i = 0; i < storedValues.length; i++) {
-                    if (storedValues[i].time == element.timeEl.text()) element.todoEl.val(storedValues[i].todo);
+                    if (storedValues[i].time == timeForTodo) todoDisplayed.val(storedValues[i].todo);
                 }
             }
         });
-
     }
 
 
-    // localStorage.clear();
-    // arr = [];
+    $(".btn-outline-primary").on("click", function (e) {
+        let btnValue = $(e.target).text();
+        if (btnValue == "Previous Day") {
+            this.startDate = startDate.subtract(1, "days");
+        }
+        if (btnValue == "Next Day") {
+            this.startDate = startDate.add(1, "days");
+        }
+        setTimes();
+        colorCode();
+        displayTodos();
+        values.forEach(function (e) {
+            console.log(e.timeVal)
+        })
+    })
 
+
+
+
+    // localStorage.clear();
 
     setTimes();
     colorCode();
